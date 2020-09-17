8b7bd46165122bd8d26ff7c24c1a142b1baed713
Samantha Csik <samanthacsik@gmail.com>
2020-09-16 22:01:05 -0700

    renamed existing scripts with number

--- code/3_filterStopWords_count_tokens.R
+++ code/3_filterStopWords_count_tokens.R
@@ -2,11 +2,9 @@
 # Summary
 ##########################################################################################
 
-# This script uses the tidytext package to filter out "stop_words" (i.e. extremely common words) from unnested token files AND count occurrances
+# This script uses the tidytext package to filter out "stop_words" (i.e. extremely common words) from unnested token files AND count occurrences
 # These unnested, filtered, and counted tokens are saved as .csv files
 # Filtering and counting takes unnested token files as inputs
-
-# !!!!!!!!!!!MAY WANT TO TRY tf-idf!!!!!!!!!!!
 
 ##########################################################################################
 # General Setup
@@ -83,7 +81,7 @@ for(i in 1:length(all_unnested_bigram_files)){
 # Filter/count TRIGRAMS
 ##########################################################################################
 
-# isolate bigram token files
+# isolate trigram token files
 all_unnested_trigram_files <- list.files(path = here::here("data", "text_mining", "unnested_tokens"), pattern = glob2rx("unnested_*Trigram*"))
 
 # remove excess columns, filter out stop_words, remove NAs, calculate counts
