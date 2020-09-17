a1f68551c554b4619bf8b2a1998af1f42380d64e
Samantha Csik <samanthacsik@gmail.com>
2020-09-16 22:02:13 -0700

    added attribute df to unnesting script from eatocsv to keep all unnested token generation in one spot

--- code/2_unnest_tokens.R
+++ code/2_unnest_tokens.R
@@ -93,3 +93,11 @@ output_csv <- function(data, names){
 # write each df as .csv file
 list(data = df_list, names = names(df_list)) %>%
      purrr::pmap(output_csv) 
+
+##############################
+# 3) save extracted attributes (from script 1b) to same directory as other unnested tokens
+##############################
+
+unnested_attributes <- read_csv(here::here("data", "attributes_query_eatocsv", "extracted_attributes", "fullQuery2020-09-13_attributes.csv"))
+
+write_csv(unnested_attributes, here::here("data", "text_mining", "unnested_tokens", "unnested_attributesIndivTokens2020-09-13.csv"))
