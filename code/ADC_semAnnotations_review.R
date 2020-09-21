8d699547662a528e67ba22c51d75747b95765f27
Samantha Csik <samanthacsik@gmail.com>
2020-09-21 15:06:18 -0700

    query set up for annotated attributes per Matt's request at arctica meeting on 2020-09-18; not working yet though

---
+++ code/ADC_semAnnotations_review.R
@@ -0,0 +1,47 @@
+# title: Current ADC Semantic Annotations Review
+# author: "Sam Csik"
+# date created: "2020-09-21"
+# date edited: "2020-09-21"
+# packages updated: __
+# R version: __
+# input: "NA"
+# output: "data/queries/*"
+
+##########################################################################################
+# Summary
+##########################################################################################
+
+##########################################################################################
+# General Setup
+##########################################################################################
+
+##############################
+# Load packages
+##############################
+
+library(dataone)
+library(tidyverse)
+library(eatocsv)
+
+##############################
+# set nodes & get token
+##############################
+
+# token reminder
+options(dataone_test_token = "...")
+
+# nodes
+cn <- CNode("PROD")
+adc_mn <- getMNode(cn, 'urn:node:ARCTIC')
+
+##########################################################################################
+# query all ADC holdings (only the most recent published version) for identifiers, titles, keywords, abstracts, and attribute info
+##########################################################################################
+
+# 1) title, keywords, abstracts
+semAnnotations_query <- query(adc_mn, 
+                              list(q = "documents:* AND obsolete:(*:* NOT obsoletedBy:*)",
+                                   fl = "identifier, title, keywords, abstract, attribute, sem_annotates, sem_annotation, sem_annotated_by, sem_comment"),
+                                   as = "data.frame")
+
+write.csv(titleKeywordsAbract_query, file = here::here("data", "ADC_semantic_annotations_review", paste("fullQuery_semAnnotations", Sys.Date(),".csv", sep = "")), row.names = FALSE) 
